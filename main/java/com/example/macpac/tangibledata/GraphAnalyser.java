075055644095bf2e2de95ded70d649d40713916e
cabeywickra <zcabcab@ucl.ac.uk>
2018-02-22 23:15:01 +0000

    Graph analyser integrated into response handler

--- main/java/com/example/macpac/tangibledata/GraphAnalyser.java
+++ main/java/com/example/macpac/tangibledata/GraphAnalyser.java
@@ -14,10 +14,11 @@ public class GraphAnalyser {
     private ArrayList<Point> points;
     private Point maxY;
     private Point minY;
+    private int graphType;
 
-    GraphAnalyser(ArrayList<Point> points){
+    GraphAnalyser(ArrayList<Point> points, int graphType){
         this.points = points;
-        analysePoints();
+        this.graphType = graphType;
     }
 
     public String getDescription() {
@@ -31,14 +32,36 @@ public class GraphAnalyser {
     private void analysePoints(){
         getInfo();
         int len = points.size()-1;
-        description += "The Graph starts at point "+points.get(0).x +" ,"+ points.get(0).y +".";
-        description += "The Graph ends at point "+points.get(len).x +" ,"+ points.get(len).y +".";
-        description += "The Graph has a maximum Y value at point "+maxY.x +" ,"+ maxY.y +".";
-        description += "The Graph has a minimum Y value at point "+minY.x +" ,"+ minY.y +".";
+        if (graphType == Graph.LINEAR_MODE) {
+            description += "Graph Type: Line Graph.";
+            description += "The line graph starts at point " + points.get(0).x + " ," + points.get(0).y + ",";
+            description += "it ends at point " + points.get(len).x + " ," + points.get(len).y + ",";
+            description += "Further, The line graph has a maximum Y value at point " + maxY.x + " ," + maxY.y + ",";
+            description += "and a minimum Y value at point " + minY.x + " ," + minY.y + ",";
+        }else if(graphType == Graph.BAR_CHART_MODE){
+            description += "Graph Type: Bar Graph.";
+            description += "The bar graph has a maximum value of " + maxY.y + ",";
+            description += "it also has a minimum value of " + minY.y ;
+            int i=0;
+            while(i <= len){
+                description += "The "+(i+1);
+                if (i==0){
+                    description += "st ";
+                }else if(i==1){
+                    description += "nd ";
+                }else if(i==2){
+                    description += "rd ";
+                }else{
+                    description+= "th ";
+                }
+                description += "bar graph has a value of " + points.get(i).y + " .";
+            }
+        }
 
     }
-    
+
     public void speak(Context context){
+        analysePoints();
         Speech.instance.Talk(context, description);
     }
 
