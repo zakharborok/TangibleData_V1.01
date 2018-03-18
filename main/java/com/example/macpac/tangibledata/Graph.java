4fa83c7c230dc611faac1e42aa1f0b15be4ba8d8
zakharborok <zaxar97@gmail.com>
2018-03-18 14:56:37 +0000

    Graph class commented

--- main/java/com/example/macpac/tangibledata/Graph.java
+++ main/java/com/example/macpac/tangibledata/Graph.java
@@ -9,17 +9,31 @@ import java.util.Collections;
 
 import static java.lang.StrictMath.floor;
 
+/**
+ * \class Graph
+ * Class to store data about currently open graph & manipulate this data.
+ */
 public class Graph
 {
-    public static Graph instance;
-    public static final int LINEAR_MODE = 0, BAR_CHART_MODE = 1;
-    public final int WIDTH, HEIGHT, X_OFFSET, Y_OFFSET;
+    public static Graph instance; /**< Global reference to a single instance of the class. */
+    public static final int LINEAR_MODE = 0/**< Constant defining graph type.*/,
+                            BAR_CHART_MODE = 1/**< Constant defining graph type.*/;
+    public final int WIDTH/**< Constant which stores pixel width of a screen.*/,
+                     HEIGHT/**< Constant which stores pixel height of a screen.*/,
+                     X_OFFSET/**< Constant which stores horizontal unit of length. Used for drawing scale e.t.c.*/,
+                     Y_OFFSET/**< Constant which stores vertical unit of length. Used for drawing scale e.t.c.*/;
+    private int type /**< Stores graph type.*/;
+    private ArrayList<Point> points /**< Stores data set.(in pixels)*/;
+    private ArrayList<Point> originalPoints/**< Stores data set.(Actual values from the csv file)*/;
+    private ArrayList<String> barChartNames/**< Stores names of bars in Bar charts*/;
 
-    private int type;
-    private ArrayList<Point> points;
-    private ArrayList<Point> originalPoints;
-    private ArrayList<String> barChartNames;
-
+    /**
+     * \brief Custom Constructor.
+     * Method to create instance of Graph with custom metrics.
+     * @param type Graph type.
+     * @param points data set.
+     * @param metrics Screen pixel size.
+     */
     public Graph(int type, ArrayList<Point> points, DisplayMetrics metrics)
     {
         this.type = type;
@@ -30,11 +44,21 @@ public class Graph
         Y_OFFSET = HEIGHT / 20;
     }
 
+    /**
+     * \brief Default Constructor.
+     * Method to create instance of Graph.
+     * @param type Graph type.
+     * @param points data set.
+     */
     public Graph(int type, ArrayList<Point> points)
     {
         this(type, points, Resources.getSystem().getDisplayMetrics());
     }
 
+    /**
+     * Method to get random data set for Linear Graphs. Used for testing.
+     * @return random data set.
+     */
     public ArrayList<Point> getExampleLinerDataPoints()
     {
         ArrayList<Point> examplePoints = new ArrayList<>();
@@ -55,6 +79,10 @@ public class Graph
         return resizeLinearPoints(examplePoints);
     }
 
+    /**
+     * Method to get random data set for Bar Graphs. Used for testing.
+     * @return random data set.
+     */
     public ArrayList<Point> getExampleBarChartDataPoints()
     {
         ArrayList<Point> examplePoints = new ArrayList<>();
@@ -89,6 +117,11 @@ public class Graph
         return resizeBarChartPoints(examplePoints);
     }
 
+    /**
+     * Method to resize data set (Linear Graphs) to match the screen.
+     * @param pointsToResize Data set.
+     * @return Resized data set.
+     */
     public ArrayList<Point> resizeLinearPoints(ArrayList<Point> pointsToResize)
     {
         if (pointsToResize.size() > 0)
@@ -140,6 +173,11 @@ public class Graph
         return pointsToResize;//*/
     }
 
+    /**
+     * Method to resize data set (Bar charts) to match the screen.
+     * @param pointsToResize Data set.
+     * @return Resized data set.
+     */
     public ArrayList<Point> resizeBarChartPoints(ArrayList<Point> pointsToResize)
     {
         if (pointsToResize.size() > 0)
@@ -170,53 +208,64 @@ public class Graph
         return pointsToResize;//*/
     }
 
-    public DisplayMetrics getMetrics()
-    {
-        return Resources.getSystem().getDisplayMetrics();
-    }
-
+    /**
+     * Getter method for graph type.
+     * @return return is graph type.
+     */
     public int getType()
     {
         return type;
     }
 
+    /**
+     * Setter method for graph type.
+     * @param type graph type.
+     */
     public void setType(int type)
     {
         this.type = type;
     }
 
+    /**
+     * Getter method for data set (resized for screen size).
+     * @return data set (resized for screen size).
+     */
     public ArrayList<Point> getPoints()
     {
         return points;
     }
 
+    /**
+     * Getter method for data set (original).
+     * @return data set (original).
+     */
     public ArrayList<Point> getOriginalPoints()
     {
         return originalPoints;
     }
 
+    /**
+     * Setter method for data set (resized).
+     * @param points data set.
+     */
     public void setPoints(ArrayList<Point> points)
     {
         this.points = points;
     }
 
+    /**
+     * Setter method for data set (resized) of Global instance of the class.
+     * @param points data set.
+     */
     public void setInstancePoints(ArrayList<Point> points)
     {
         Graph.instance.points = points;
     }
 
-    private ArrayList<Point> copyPpints(ArrayList<Point> points)
-    {
-        ArrayList<Point> copy_point = new ArrayList();
-
-        for (Point p : points)
-        {
-            copy_point.add(new Point(p.x, p.y));
-        }
-
-        return copy_point;
-    }
-
+    /**
+     * Setter method for data set (original).
+     * @param originalPoints data set.
+     */
     public void setOriginalPoints(ArrayList<Point> originalPoints)
     {
         int x,y;
@@ -229,11 +278,19 @@ public class Graph
         this.originalPoints = copyList;
     }
 
+    /**
+     * Setter method for names of bar in bar charts.
+     * @param barChartNames bar chart names.
+     */
     public void setBarChartNames(ArrayList<String> barChartNames)
     {
         this.barChartNames = barChartNames;
     }
 
+    /**
+     * Getter method for names of bar in bar charts.
+     * @return names of bar in bar charts.
+     */
     public ArrayList<String> getBarChartNames()
     {
         return barChartNames;
